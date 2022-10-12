b29ce914f038ef12ed17cd82ad3bddff6ebb6f3f
FelixvL <digitaleservice@gmail.com>
2022-10-12 16:11:35 +0200

    PE

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 
-class Demo{ // PD
+class Demo{ // PE
 	public static void main(String[] args){
 		Dier d = new Kip();
 		d.lopen();
@@ -12,9 +12,20 @@ class Dier{
 }
 
 class Kip extends Dier{
-	int lopen(){
+	int lopen(int a){
 		System.out.println("kip");
 		return 5;
 	}
 }
 
+/*
+overriden
+1 signature hetzelfde
+2 returntype covariant
+3 accessmodifier mag niet vernauwen
+4 throws clausule mag covariant bevatten OF NIETS
+
+overload
+1 Signature moet anders zijn   (aantal type volgorde)
+
+*/
