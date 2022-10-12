9443efb309ea8b83812bf71339e051098b2ceba0
FelixvL <digitaleservice@gmail.com>
2022-10-12 15:49:51 +0200

    OY

--- Demo.java
+++ Demo.java
@@ -1,10 +1,20 @@
 
-class Demo{ // OX
+class Demo{ // OY
 	public static void main(String[] args){
-		char getal = 70;
-		double getal2 = getal;
-		System.out.println(getal2);
+		Scherm s = new Scherm();
+		s.verstelHoogte();
+		System.out.println(s.hoogte);
 	}
+}
+class Scherm{
+	int hoogte;
+	Scherm(){
+		System.out.println("Ik zie dingen");
+	}
+	void verstelHoogte(){
+		System.out.println("ff verstellen");
+	}
+
 }
 
 
