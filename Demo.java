7cd3244f7e39d6aa8dd01168826bb001c386c01b
FelixvL <digitaleservice@gmail.com>
2022-10-12 16:44:47 +0200

    PH

--- Demo.java
+++ Demo.java
@@ -1,36 +1,20 @@
 
-class Demo{ // PG
+class Demo{ // PH
 	public static void main(String[] args){
-		Boerderij bd = new Boerderij();
-		Dier d = bd.fokken();
-		d.lopen();
+		D d = new D();
+		A y = d.en();
+		y.c();
 	}
 }
-class Boerderij{
-	Dier fokken(){
-		System.out.println("er wordt een dier gemaakt");
-		return new Kip();
-	}
-}
-class Dier{
-	Dier(){
-		System.out.println("aha Dier");
-	}
-	void lopen(){
-		System.out.println("lopen in dier");
-	}
-}
-class Kip extends Dier{
-	Kip(){
-		System.out.println("aha Kip");
-	}
-	void lopen(){
-		System.out.println("lopen in Kip");
-	}
-	int lopen(int a){
-		System.out.println("kip");
-		return 5;
+class A{
+	void c(){
+		System.out.println("in c");
 	}
 }
 
-
+class D{
+	A en(){
+		System.out.println("in en");
+		return new A();
+	}
+}
