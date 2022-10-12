2435bc0c3476f6639ae92265e238874c8166bdc5
FelixvL <digitaleservice@gmail.com>
2022-10-12 16:31:39 +0200

    PF

--- Demo.java
+++ Demo.java
@@ -1,31 +1,36 @@
 
-class Demo{ // PE
+class Demo{ // PF
 	public static void main(String[] args){
-		Dier d = new Kip();
+		Boerderderij bd = new Boerderij();
+		Dier d = bd.fokken();
 		d.lopen();
 	}
 }
-class Dier{
-	void lopen(){
-		System.out.println("Dier");
+class Boerderij{
+	Dier fokken(){
+		System.out.println("er wordt een dier gemaakt");
+		return new Kip();
 	}
 }
-
+class Dier{
+	Dier(){
+		System.out.println("aha Dier");
+	}
+	void lopen(){
+		System.out.println("lopen in dier");
+	}
+}
 class Kip extends Dier{
+	Kip(){
+		System.out.println("aha Kip");
+	}
+	void lopen(){
+		System.out.println("lopen in Kip");
+	}
 	int lopen(int a){
 		System.out.println("kip");
 		return 5;
 	}
 }
 
-/*
-overriden
-1 signature hetzelfde
-2 returntype covariant
-3 accessmodifier mag niet vernauwen
-4 throws clausule mag covariant bevatten OF NIETS
 
-overload
-1 Signature moet anders zijn   (aantal type volgorde)
-
-*/
