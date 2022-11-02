0c3a205f1f8499ebf831c0437df14fe3c466040b
FelixvL <digitaleservice@gmail.com>
2022-11-02 12:59:55 +0100

    AJ

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 
-class Demo{ // AI
-	public static void main(String[] args){
+class Demo{ // AJ
+	public static void main(String[] args)throws Exception{
 		Go hond = new Hond();
 		hond.lopen();
 		hond.lopen(25);
@@ -9,7 +9,7 @@ class Demo{ // AI
 }
 
 interface Go{
-	int a;
+	int a = 45;
 	public abstract void lopen();
 }
 
@@ -18,7 +18,7 @@ class Hond implements Go{
 	public void lopen(){
 		System.out.println("lopen met" + a);
 	}
-	int lopen(int r){
+	int lopen(int r) throws Exception{
 		System.out.println("in lopen met r");
 		return 17;
 	}
