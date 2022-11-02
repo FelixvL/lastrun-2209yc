ee9108b799fd472fcb65385bef6b210a24559610
FelixvL <digitaleservice@gmail.com>
2022-11-02 12:41:12 +0100

    AE

--- Demo.java
+++ Demo.java
@@ -1,17 +1,27 @@
 
-class Demo{ // AD
+class Demo{ // AE
 	public static void main(String[] args){
 		Gereedschap h = new HooiVork();
 		System.out.println(h.b);
+		h.draaien();
 	}
 }
 
 class Gereedschap{
-	int b = 14;
+	int b;
+	Gereedschap(){
+		System.out.println("in g");
+	}
+	void draaien(){
+		System.out.println("oei duizelig");
+	}
 }
 class HooiVork extends Gereedschap{
 	int b = 5;	
 	HooiVork(){
 		System.out.println("ooit weer" + b);
+	}
+	void draaien(){
+		System.out.println("oei heel duizelig");
 	}
 }
