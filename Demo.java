7145f8332b8d3aa5f56913c2b0ba6ae59f6d83b3
FelixvL <digitaleservice@gmail.com>
2022-11-02 12:44:48 +0100

    AF

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 
-class Demo{ // AE
+class Demo{ // AF
 	public static void main(String[] args){
 		Gereedschap h = new HooiVork();
 		System.out.println(h.b);
@@ -8,7 +8,7 @@ class Demo{ // AE
 }
 
 class Gereedschap{
-	int b;
+	final int b;
 	Gereedschap(){
 		System.out.println("in g");
 	}
@@ -19,7 +19,11 @@ class Gereedschap{
 class HooiVork extends Gereedschap{
 	int b = 5;	
 	HooiVork(){
+		this(4);
 		System.out.println("ooit weer" + b);
+	}
+	HooiVork(int b){
+		System.out.println("tweede");
 	}
 	void draaien(){
 		System.out.println("oei heel duizelig");
