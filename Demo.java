872bd1087abe5c0e7a32cf21f9dd5f520a89b9d6
FelixvL <digitaleservice@gmail.com>
2022-11-02 13:03:53 +0100

    AK

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 
-class Demo{ // AJ
+class Demo{ // AK
 	public static void main(String[] args)throws Exception{
-		Go hond = new Hond();
+		Hond hond = new Hond();
 		hond.lopen();
 		hond.lopen(25);
 		System.out.println(hond.a);
@@ -15,10 +15,10 @@ interface Go{
 
 class Hond implements Go{
 
-	public void lopen(){
+	public void lopen() throws Exception{
 		System.out.println("lopen met" + a);
 	}
-	int lopen(int r) throws Exception{
+	int lopen(int r){
 		System.out.println("in lopen met r");
 		return 17;
 	}
