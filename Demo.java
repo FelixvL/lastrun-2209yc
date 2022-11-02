92c6322f90b6a1162ed39ca1258c8079fd35c5cf
FelixvL <digitaleservice@gmail.com>
2022-11-02 12:58:42 +0100

    AI

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 
-class Demo{ // AH
+class Demo{ // AI
 	public static void main(String[] args){
 		Go hond = new Hond();
 		hond.lopen();
@@ -18,7 +18,7 @@ class Hond implements Go{
 	public void lopen(){
 		System.out.println("lopen met" + a);
 	}
-	void lopen(int r){
+	int lopen(int r){
 		System.out.println("in lopen met r");
 		return 17;
 	}
